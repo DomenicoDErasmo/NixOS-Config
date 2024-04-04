616eaa9a24539e7f4c2c635ff8c4393f113f8854
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2024-04-03 21:45:19 -0400

    added rust-analyzer to shell

--- shells/rust/shell.nix
+++ shells/rust/shell.nix
@@ -9,6 +9,7 @@ in
       pkgs.rustc
       pkgs.rustfmt
       pkgs.clippy
+      pkgs.rust-analyzer
     ];
 
     # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
