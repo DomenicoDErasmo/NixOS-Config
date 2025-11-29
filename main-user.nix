e47e41c29837fbdc6c075f5b7e3e2ec86393c74a
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-11-29 12:43:39 -0500

    home manager

--- main-user.nix
+++ main-user.nix
@@ -3,11 +3,13 @@
   config,
   pkgs,
   ...
-}: {
-  options = {
-    main-user.enable = lib.mkEnableOption "enable user module";
+}: let
+  cfg = config.main-user;
+in {
+  options.main-user = {
+    enable = lib.mkEnableOption "enable user module";
 
-    main-user.userName = lib.mkOption {
+    userName = lib.mkOption {
       default = "domenico";
       description = ''
         username
@@ -15,12 +17,12 @@
     };
   };
 
-  config = lib.mkIf cnofig.main-user.enable {
-    users.users.${config.main-user.userName} = {
+  config = lib.mkIf cfg.enable {
+    users.users.${cfg.userName} = {
       isNormalUser = true;
       initialPassword = "domo";
       description = "main user";
-      shell = pkgs.bash;
+      extraGroups = ["networkmanager" "wheel"];
     };
   };
 }
