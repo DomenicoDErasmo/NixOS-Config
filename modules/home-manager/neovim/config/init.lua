93ff5545f99e63a3eebf2abad1e6c61f45229686
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-07 12:56:04 -0500

    fix init lua

--- modules/home-manager/neovim/config/init.lua
+++ modules/home-manager/neovim/config/init.lua
@@ -1,34 +1,33 @@
 vim.pack.add {
-    { src = "https://github.com/neovim/nvim-lspconfig"}
-    { src = "https://github.com/mason-org/mason.nvim"}
-    { src = "https://github.com/mason-org/mason-lspconfig.nvim"}
-    { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"}
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+    { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
 }
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-tool-installer").setup({
+require('mason').setup()
+require('mason-lspconfig').setup()
+require('mason-tool-installer').setup({
     ensure_installed = {
-        "lua_ls",
-        "stylua",
+        'lua_ls',
+        'stylua',
     }
 })
 
--- Extending an existing config
-vim.lsp.config.("lua_ls", {
+vim.lsp.config.('lua_ls', {
     settings = {
         Lua = {
             runtime = {
-                version = "LuaJIT",
+                version = 'LuaJIT',
             },
             diagnostics = {
                 globals = {
-                    "vim",
-                    "require"
+                    'vim',
+                    'require'
                 },
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true)
+                library = vim.api.nvim_get_runtime_file('', true)
             },
             telemetry = {
                 enable = false,
