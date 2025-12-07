171d029e2e02546f2d93c448984b2f7424be6892
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-06 21:43:56 -0500

    workspace configs

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -16,8 +16,15 @@
         "$mod, Q, exec, ghostty"
         "$mod, W, exec, waybar"
         "$mod, C, killactive"
-        "ALT, TAB, exec, rofi -show drun -show-icons"
         "$mod, M, exit"
+        "ALT, TAB, exec, rofi -show drun -show-icons"
+        "$mod SHIFT, right, movetoworkspace, +1"
+        "$mod SHIFT, left, movetoworkspace, -1"
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
       ];
       # TODO: how to un-hardcode?
       monitor = [
@@ -25,13 +32,13 @@
         "HDMI-A-1,2560x1440@143.98,2560x0,1"
       ];
 
-      # workspace = [
-      #   "1,monitor:DP-3,default:true"
-      #   "2,monitor:DP-3"
-      #   "3,monitor:DP-3"
-      #   "4,monitor:DP-3"
-      #   "5,monitor:DP-3"
-      # ];
+      workspace = [
+        "1,monitor:DP-3,default:true"
+        "2,monitor:DP-3"
+        "3,monitor:HDMI-A-1"
+        "4,monitor:HDMI-A-1"
+        "5,monitor:HDMI-A-1"
+      ];
     };
   };
 }
