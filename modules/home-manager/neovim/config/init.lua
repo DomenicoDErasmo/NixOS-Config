1c6efd9764f11b8b391a464dd6f1bcd0d945ccc7
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-07 13:29:29 -0500

    basic lua LSP

--- modules/home-manager/neovim/config/init.lua
+++ modules/home-manager/neovim/config/init.lua
@@ -1,37 +1,52 @@
+-- ==========================================
+-- Plugins
+-- ==========================================
 vim.pack.add {
+    -- LSP config
     { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/mason-org/mason.nvim' },
-    { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-    { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
+    -- required by none-ls
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://github.com/nvimtools/none-ls.nvim' },
 }
 
-require('mason').setup()
-require('mason-lspconfig').setup()
-require('mason-tool-installer').setup({
-    ensure_installed = {
-        'lua_ls',
-        'stylua',
-    }
-})
+-- ==========================================
+-- Paths to Nix-installed binaries
+-- ==========================================
+local lspconfig = require("lspconfig")
+local lua_ls_cmd = os.getenv("HOME") .. "/.nix-profile/bin/lua-language-server"
+local stylua_cmd = os.getenv("HOME") .. "/.nix-profile/bin/stylua"
 
-vim.lsp.config('lua_ls', {
+-- ==========================================
+-- LSP Configuration
+-- ==========================================
+
+-- deprecated version seems to work
+lspconfig.lua_ls.setup({
+    cmd = { lua_ls_cmd },
     settings = {
         Lua = {
-            runtime = {
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                globals = {
-                    'vim',
-                    'require'
-                },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file('', true)
-            },
-            telemetry = {
-                enable = false,
-            },
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim", "require" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            telemetry = { enable = false },
         },
     },
 })
+
+-- ==========================================
+-- Formatter using null-ls
+-- ==========================================
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua.with({
+            command = stylua_cmd,  -- Use Nix-installed stylua
+        }),
+    },
+})
+
+-- Optional: create :Format command
+vim.api.nvim_create_user_command('Format', function()
+    vim.lsp.buf.format({ async = true })
+end, {})
--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -3,6 +3,15 @@
   pkgs,
   ...
 }: {
+  home.packages = [
+    # Unzip compressed files (for stylua in Neovim)
+    pkgs.unzip
+    # Lua formatting
+    pkgs.stylua
+    # Lua LSP
+    pkgs.lua-language-server
+  ];
+
   programs.neovim = {
     enable = true;
     package = inputs.neovimNightlyOverlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
