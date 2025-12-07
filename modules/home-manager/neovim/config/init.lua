f1ae43aa020740ca4283ed38651047ad9d152d9e
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-07 15:37:50 -0500

    move null_ls (formatting wrapper) into separate file

--- modules/home-manager/neovim/config/init.lua
+++ modules/home-manager/neovim/config/init.lua
@@ -4,13 +4,13 @@ require("config.cmp")
 require("config.colorscheme")
 require("config.treesitter")
 require("config.keymap")
+require("config.null_ls")
 require("config.lsp.lua")
 
 
 -- ==========================================
 -- Paths to Nix-installed binaries
 -- ==========================================
-local stylua_cmd = os.getenv("HOME") .. "/.nix-profile/bin/stylua"
 local rustfmt_cmd = os.getenv("HOME") .. "/.nix-profile/bin/rustfmt"
 
 -- ==========================================
@@ -34,21 +34,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.lsp.start(vim.lsp.config["rust_analyzer"])
   end,
 })
-
--- ==========================================
--- Formatter using null-ls
--- ==========================================
-local null_ls = require("null-ls")
-
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua.with({
-			command = stylua_cmd,
-		}),
-	},
-})
-
--- Optional: create :Format command
-vim.api.nvim_create_user_command("Format", function()
-	vim.lsp.buf.format({ async = true })
-end, {})
---
+++ modules/home-manager/neovim/config/lua/config/null_ls.lua
@@ -0,0 +1,17 @@
+
+local stylua_cmd = os.getenv("HOME") .. "/.nix-profile/bin/stylua"
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.stylua.with({
+			command = stylua_cmd,
+		}),
+	},
+})
+
+-- Optional: create :Format command
+vim.api.nvim_create_user_command("Format", function()
+	vim.lsp.buf.format({ async = true })
+end, {})
