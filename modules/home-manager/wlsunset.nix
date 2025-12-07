6749a8e3884f026a49c56a230b9b6d7f77249821
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-06 20:53:42 -0500

    fix args

--- modules/home-manager/wlsunset.nix
+++ modules/home-manager/wlsunset.nix
@@ -1,10 +1,17 @@
 {pkgs, ...}: {
   systemd.user.services.wlsunset = {
-    description = "Wayland color temperature adjustment";
-    serviceConfig = {
-      ExecStart = "${pkgs.wlsunset}/bin/wlsunset -l 40.7128 -L -74.0060 -t 6500:4000 -m 0.0:1.0 -s 5";
-      Type = "simple";
+    Unit = {
+      Description = "Adjust screen color temperature automatically";
     };
-    wantedBy = ["default.target"];
+
+    Service = {
+      ExecStart = "${pkgs.wlsunset}/bin/wlsunset -l 40.7128 -L -74.0060 -t 3500 -T 6500";
+      Type = "simple";
+      Restart = "always";
+    };
+
+    Install = {
+      WantedBy = ["default.target"];
+    };
   };
 }
