dc838bb23ee0132267e52e390200b9032487c3ea
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-07 09:57:24 -0500

    move settings into separate JSON

---
+++ modules/home-manager/vscode/vscode.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  # VSCode -code editor
+  programs.vscode = {
+    enable = true;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        kamadorueda.alejandra # Alejandra integration
+        arrterian.nix-env-selector # Select nix shells from VS Code
+        jnoortheen.nix-ide # Nix IDE
+        timonwong.shellcheck # ShellCheck
+        eamodio.gitlens # GitLens - more Git context
+        rust-lang.rust-analyzer # rust-analyzer - Rust LSP
+        vadimcn.vscode-lldb # LLDB - Debugging
+        tamasfe.even-better-toml # TOML syntax highlighting
+        fill-labs.dependi # Cargo crate completion
+        llvm-vs-code-extensions.vscode-clangd # clangd
+      ];
+
+      userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+    };
+  };
+}
