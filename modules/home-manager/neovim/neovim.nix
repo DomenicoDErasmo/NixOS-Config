681074b6306cfff84060bf1eb5a85aa20ca34909
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-07 12:30:43 -0500

    one more test

--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -5,7 +5,7 @@
 }: {
   programs.neovim = {
     enable = true;
-    package = inputs.neovimNightlyOverlay.packages.${pkgs.system}.default;
-    config = builtins.readFile ./config/init.lua;
+    package = inputs.neovimNightlyOverlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
+  home.file."~/.config/nvim".source = ./config;
 }
