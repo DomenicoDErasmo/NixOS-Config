9d964ccb9da8ddb1c5dcce8ba7141728460fcf37
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-08 19:42:53 -0500

    add telescope

--- modules/home-manager/neovim/config/lua/config/plugins.lua
+++ modules/home-manager/neovim/config/lua/config/plugins.lua
@@ -4,4 +4,6 @@ vim.pack.add({
 	-- required by none-ls
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvimtools/none-ls.nvim" },
+	-- telescope - customizable fuzzy finder
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 })
