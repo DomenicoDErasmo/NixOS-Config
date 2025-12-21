682aec726c213e02471215abc39a9abd4dcc2cbb
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-21 16:28:50 -0500

    add rainbow delimiters

--- modules/home-manager/neovim/config/lua/config/rainbow_delimiters.lua
+++ modules/home-manager/neovim/config/lua/config/rainbow_delimiters.lua
@@ -1,15 +1,15 @@
 vim.pack.add({
-    { src = "https://github.com/HiPhish/rainbow-delimiters.nvim" },
+	{ src = "https://github.com/HiPhish/rainbow-delimiters.nvim" },
 })
 
 vim.g.rainbow_delimiters = {
-    highlight = {
-        "RainbowDelimiterRed",
-        "RainbowDelimiterYellow",
-        "RainbowDelimiterBlue",
-        "RainbowDelimiterOrange",
-        "RainbowDelimiterGreen",
-        "RainbowDelimiterViolet",
-        "RainbowDelimiterCyan",
-    },
+	highlight = {
+		"RainbowDelimiterRed",
+		"RainbowDelimiterYellow",
+		"RainbowDelimiterBlue",
+		"RainbowDelimiterOrange",
+		"RainbowDelimiterGreen",
+		"RainbowDelimiterViolet",
+		"RainbowDelimiterCyan",
+	},
 }
