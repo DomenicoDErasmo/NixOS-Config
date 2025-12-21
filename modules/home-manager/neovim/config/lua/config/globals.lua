07899afb78ddbfe8f8a0f73e112505dcc52e0ddd
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-21 16:26:03 -0500

    add rainbow delimiters

--- modules/home-manager/neovim/config/lua/config/globals.lua
+++ modules/home-manager/neovim/config/lua/config/globals.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.o.number = true
 vim.o.relativenumber = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
---
+++ modules/home-manager/neovim/config/lua/config/rainbow_delimiters.lua
@@ -0,0 +1,15 @@
+vim.pack.add({
+    { src = "https://github.com/HiPhish/rainbow-delimiters.nvim" },
+})
+
+vim.g.rainbow_delimiters = {
+    highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+    },
+}
