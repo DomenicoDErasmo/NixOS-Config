98fbab7f4ff2e549d8f47053ea4a222fdd4fcea2
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-21 16:37:36 -0500

    add more file types to treesitter

--- modules/home-manager/neovim/config/lua/config/treesitter.lua
+++ modules/home-manager/neovim/config/lua/config/treesitter.lua
@@ -2,13 +2,14 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "lua", "rust" },
+require("nvim-treesitter.configs").setup({
+    ensure_installed = { "lua", "rust", "cpp", "python", "css", "cmake", "markdown", "nix", "bash" },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
     indent = {
-        enable = true
-    }
-}
+        enable = true,
+    },
+})
+
