01263cf0ed5905355442fa3ad69c6756cf0e3a3c
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-24 11:00:43 -0500

    fix LSP

--- modules/home-manager/neovim/config/lua/config/null_ls.lua
+++ modules/home-manager/neovim/config/lua/config/null_ls.lua
@@ -1,12 +1,10 @@
-local stylua_cmd = os.getenv("HOME") .. "/.nix-profile/bin/stylua"
-
 local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
         -- Lua
         null_ls.builtins.formatting.stylua.with({
-            command = stylua_cmd,
+            command = "stylua",
         }),
 
         -- Python
