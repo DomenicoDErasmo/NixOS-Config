22102d387e1add32960b517709c0541f45bf7f7b
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-28 14:21:58 -0500

    format

--- modules/home-manager/neovim/config/lua/config/treesitter.lua
+++ modules/home-manager/neovim/config/lua/config/treesitter.lua
@@ -1,15 +1,14 @@
 vim.pack.add({
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 require("nvim-treesitter").setup({
-    ensure_installed = { "lua", "rust", "cpp", "python", "css", "cmake", "markdown", "nix", "bash" },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
-    },
+  ensure_installed = { "lua", "rust", "cpp", "python", "css", "cmake", "markdown", "nix", "bash" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
 })
-
