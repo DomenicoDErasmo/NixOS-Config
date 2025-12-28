f0fe7e9985399301b8a0c14a97c5d5d7bbbbc438
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2025-12-28 11:54:53 -0500

    format

--- modules/home-manager/neovim/config/lua/config/keymap.lua
+++ modules/home-manager/neovim/config/lua/config/keymap.lua
@@ -1,23 +1,23 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local buf = args.buf
-        local opts = { buffer = buf, remap = false }
+  callback = function(args)
+    local buf = args.buf
+    local opts = { buffer = buf, remap = false }
 
-        -- Go to definition
-        -- Related, jump lists:
-        -- Ctrl+o to jump back and Ctrl-i to jump forward
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        -- Hover documentation
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        -- References
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        -- Type definition
-        vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
-        -- Rename symbol
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        -- Signature help (in insert mode)
-        vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
-    end,
+    -- Go to definition
+    -- Related, jump lists:
+    -- Ctrl+o to jump back and Ctrl-i to jump forward
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    -- Hover documentation
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    -- References
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    -- Type definition
+    vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+    -- Rename symbol
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    -- Signature help (in insert mode)
+    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+  end,
 })
 
 -- Show diagnostic in a floating window
