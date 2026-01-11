e48d690e8b306c3e7429ada14de78bd5f042731c
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2026-01-11 14:07:25 -0500

    add debugger support

--- modules/dev_templates/cpp_shell/flake.nix
+++ modules/dev_templates/cpp_shell/flake.nix
@@ -24,8 +24,8 @@
       ];
 
       shellHook = ''
-        export CC=${pkgs.clang}/bin/clang
-        export CXX=${pkgs.clang}/bin/clang++
+        export CC=${pkgs.llvmPackages_21.clang}/bin/clang
+        export CXX=${pkgs.llvmPackages_21.clang}/bin/clang++
 
         export C_INCLUDE_PATH=$C_INCLUDE_PATH:${pkgs.glibc.dev}/include
         export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${pkgs.glibc.dev}/include
@@ -33,10 +33,12 @@
         export LIBCXX_INCLUDE_DIR=${pkgs.llvmPackages_21.libcxx.dev}/include/c++/v1
         export LIBCXX_LIB_DIR=${pkgs.llvmPackages_21.libcxx.out}/lib
 
-        export PATH=${pkgs.clang}/bin:$PATH
+        export PATH=${pkgs.llvmPackages_21.clang}/bin:$PATH
 
         export GTEST_ROOT=${pkgs.gtest}
         export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$GTEST_ROOT
+
+        export PATH=${pkgs.llvmPackages_21.lldb}/bin:$PATH
 
         if [ -z "$ZSH_VERSION" ]; then
           exec zsh
