a84b660af4e50515438e5903cb1b9c34a5f6da83
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2026-01-11 01:51:57 -0500

    fix Treesitter issue

--- modules/home-manager/neovim/config/lua/config/plugins.lua
+++ modules/home-manager/neovim/config/lua/config/plugins.lua
@@ -1,19 +1,17 @@
 vim.pack.add({
-	-- LSP config
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	-- required by none-ls
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvimtools/none-ls.nvim" },
-	-- telescope - customizable fuzzy finder
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	-- dev icons
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	-- git signs
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	-- status bar
-	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-	-- TODO highlighting
-	{ src = "https://github.com/folke/todo-comments.nvim" },
-	-- Syntax parser
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  -- LSP config
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  -- required by none-ls
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvimtools/none-ls.nvim" },
+  -- telescope - customizable fuzzy finder
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  -- dev icons
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  -- git signs
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  -- status bar
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+  -- TODO highlighting
+  { src = "https://github.com/folke/todo-comments.nvim" },
 })
--- modules/home-manager/neovim/config/lua/config/treesitter.lua
+++ modules/home-manager/neovim/config/lua/config/treesitter.lua
@@ -1,10 +1,19 @@
-require("nvim-treesitter.config").setup({
-	ensure_installed = { "lua", "rust", "cpp", "python", "css", "cmake", "markdown", "nix", "bash" },
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
+vim.pack.add({
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+})
+
+local ok, treesitter = pcall(require, "nvim-treesitter.config")
+if not ok then
+  treesitter = require("nvim-treesitter.configs")
+end
+
+treesitter.setup({
+  ensure_installed = { "lua", "rust", "cpp", "python", "css", "cmake", "markdown", "nix", "bash" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
 })
