b44c0bb59aaf870eca14e3942dd09f11d1bab1e8
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2026-03-03 19:02:13 -0500

    obsididan

---
+++ modules/home-manager/common/neovim/config/lua/config/obsidian.lua
@@ -0,0 +1,19 @@
+require("obsidian").setup({
+  mappings = {
+    -- Follow link
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+
+    -- Hover preview of linked file
+    ["K"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+  },
+})
--- modules/home-manager/common/neovim/config/lua/config/plugins.lua
+++ modules/home-manager/common/neovim/config/lua/config/plugins.lua
@@ -1,17 +1,24 @@
 vim.pack.add({
-	-- LSP config
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	-- required by none-ls
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvimtools/none-ls.nvim" },
-	-- telescope - customizable fuzzy finder
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	-- dev icons
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	-- git signs
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	-- status bar
-	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-	-- TODO highlighting
-	{ src = "https://github.com/folke/todo-comments.nvim" },
+  -- LSP config
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  -- required by none-ls
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvimtools/none-ls.nvim" },
+  -- telescope - customizable fuzzy finder
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  -- dev icons
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  -- git signs
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  -- status bar
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+  -- TODO highlighting
+  { src = "https://github.com/folke/todo-comments.nvim" },
+  -- Markdown rendering
+  { src = "https://github.com/nvim-mini/mini.nvim" },
+  { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+  -- Obsidian support in Neovim
+  { src = "https://github.com/epwalsh/obsidian.nvim" },
+  -- Floating previews
+  { src = "https://github.com/toppair/peek.nvim" },
 })
