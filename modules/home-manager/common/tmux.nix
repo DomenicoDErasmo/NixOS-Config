387280d8a4b79c41a05469ecbda2318812939d81
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2026-04-13 22:33:35 -0400

    some tmux settings

--- modules/home-manager/common/tmux.nix
+++ modules/home-manager/common/tmux.nix
@@ -5,6 +5,9 @@
     prefix = "C-Space";
     extraConfig = ''
       bind C-Space send-prefix
+      set-option -g default-terminal "screen-256color"
+      set-option -sg escape-time 10
+      set-option -g focus-events on
     '';
   };
 }
